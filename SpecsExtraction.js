d815e1531cb7968c955671b9f13f1e1d2234207c
PIYUSH <me.piyush.360@gmail.com>
2020-05-19 01:00:42 +0530

    Lacks Identification and scraping of the required data

---
+++ SpecsExtraction.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const fs = require('fs');
+const jsdom = require('jsdom');
+const{JSDOM} = jsdom;
+const links = require('./LaptopLinks.json');
+
+//gives count for iteration
+let c = Object.keys(links).length;
+//looping through object
+async function SpecsExtractor(){
+    let i = 1;
+    for(x in links){
+        //limits it to just one iteration
+        if(i<2){
+            console.log(links[x]); 
+            console.log(x);
+           // await axios.get(links[x])
+            i++;
+        }else{
+            break;
+        }
+    }
+}
+
+SpecsExtractor();
