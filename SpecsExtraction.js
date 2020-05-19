a7efbcaa03361ace77686be198ae9ebb3dcb1148
PIYUSH <me.piyush.360@gmail.com>
2020-05-19 18:14:37 +0530

    Features of laptops can be accessed and are being printed. Lines 33-36 needs to be uncommented so we can store the features.

--- SpecsExtraction.js
+++ SpecsExtraction.js
@@ -3,21 +3,55 @@ const fs = require('fs');
 const jsdom = require('jsdom');
 const{JSDOM} = jsdom;
 const links = require('./LaptopLinks.json');
+const specs = require('./Specs.json');
 
 //gives count for iteration
 let c = Object.keys(links).length;
+
 //looping through object
 async function SpecsExtractor(){
-    let i = 1;
+    let i = 0;
     for(x in links){
         //limits it to just one iteration
-        if(i<2){
-            console.log(links[x]); 
+        if(i<1){
+            // console.log(links[x]); 
             console.log(x);
+            
             await axios.get(links[x])
                         .then((response)=>{
                             let frag = JSDOM.fragment(String(response.data));
+
+                            //Extract price
+                            let price = frag.querySelector(".price").textContent;
                             
+                            price = price.replace('₹','');
+                            while(price.search(',') != -1){
+                               price =  price.replace(",","");
+                            }
+                            price = parseInt(price);
+                            console.log(price);
+                            //inserting name and price
+                            // specs[`${i}`]= {};
+                            // specs[`${i}`]["Name"] = x;
+                            // specs[`${i}`]["Price"] = price;
+                            for(let i = 0;;i++){
+                                if(frag.querySelector(".specs-table tr .bold")){
+                                   console.log(frag.querySelector(".specs-table tr .bold").textContent);
+                                   console.log(frag.querySelector(".specs-table tr .bold ~ td").textContent)
+                                   console.log('|-------------------------------------|')
+                                   frag.querySelector(".specs-table tr .bold ~ td").remove();
+                                   frag.querySelector(".specs-table tr .bold").remove();
+                                  
+                                }else{
+                                    break;
+                                }
+                            }
+                            
+
+                            fs.writeFileSync('Specs.json',JSON.stringify(specs));
+                            
+
+                           
                         }).catch((error)=>{
                             console.log(error);
                         })
